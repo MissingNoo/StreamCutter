a144a85a978cc6c813e89034a0c711027da0e6ba
Airgeadlamh <eduaraguia@gmail.com>
2024-10-20 18:11:41 -0300

    Add upload to youtube option. Update example config

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ stream_name = data[0].strip().split(";")[1]
 stream_link = data[1].strip().split(";")[1]
 streamer_name = data[2].strip().split(";")[1]
 mp3_image = data[3].strip().split(";")[1]
+upload_to_youtube = data[4].strip().split(";")[1]
+del data[4]
 del data[3]
 del data[2]
 del data[1]
@@ -30,10 +32,12 @@ print("Song Data: " + str(songs))
 
 #download stream
 #stream_file = "stream.mp4"
-stream_file = "/tmp/stream/" + stream_name + ".mp4"
+stream_file = "/tmp/stream/" + stream_name + ".mkv"
 if not os.path.isfile(stream_file):
     print("Downloading stream, please wait")
+    stream_file = "/tmp/stream/" + stream_name
     ytdl = shell(['yt-dlp', stream_link, '-o', stream_file])
+    stream_file = "/tmp/stream/" + stream_name + ".mkv"
     print("Stream downloaded, processing songs")
 else:
     print("Stream file exists, processing songs")
@@ -45,13 +49,15 @@ shell(['mkdir', '-p', "output/" + stream_name + "/mp3"])
 shell(['mkdir', '-p', "output/" + stream_name + "/mp4"])
 song_num = 1
 for song in songs:
-    if not os.path.isfile("output/" + stream_name + "/mp4/" + song[0] + ".mp4"): #MP4 Version
-        ff = shell(['ffmpeg', '-y', '-ss', song[1], '-to', song[2], '-i', stream_file, '-c', 'copy', "output/" + stream_name + "/mp4/" + song[0] + ".mp4"])
+    mp3_path = "output/" + stream_name + "/mp3/" + song[0] + ".mp3"
+    mp4_path = "output/" + stream_name + "/mp4/" + song[0] + ".mp4"
+    if not os.path.isfile(mp4_path): #MP4 Version
+        ff = shell(['ffmpeg', '-y', '-ss', song[1], '-to', song[2], '-i', stream_file, '-c', 'copy', mp4_path])
         if str(ff.returncode) != "0":
             print("Error cutting song: " + song[0])
     if not os.path.isfile("output/" + stream_name + "/mp3/" + song[0] + ".mp3"): #MP3 Version
-        ff = shell(['ffmpeg', '-y', '-i', "output/" + stream_name + "/mp4/" + song[0] + ".mp4", "output/" + stream_name + "/mp3/" + song[0] + ".mp3"])
-        tg = music_tag.load_file("output/" + stream_name + "/mp3/" + song[0] + ".mp3")
+        ff = shell(['ffmpeg', '-y', '-i', mp4_path, mp3_path])
+        tg = music_tag.load_file(mp3_path)
         tg['title'] = song[0].split(" - ")[0].replace(" ", "|").strip().replace("|", " ")
         tg['title'] = song[0].split(" - ")[0].replace(" ", "|").strip().replace("|", " ")
         tg['artist'] = streamer_name
@@ -64,4 +70,6 @@ for song in songs:
         tg.save()
         if str(ff.returncode) != "0":
             print("Error cutting song: " + song[0])
+    if upload_to_youtube == "yes":
+        up = shell(['./yt-upload', '[' + streamer_name + ']' + ' ' + song[0], stream_link, mp4_path])
 print("All songs saved and tags applied")
